db26f6ffaffdc187c727e9dd35cbc9605fa65801
amandaholt-bwell <amanda.holt@icanbwell.com>
2022-02-16 11:58:57 -0500

    Removing what should be branched

--- polls/models.py
+++ polls/models.py
@@ -5,18 +5,10 @@ from django.utils import timezone
 
 from django.contrib import admin
 
-class Choice(models.Model):
-    #question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    choices = models.ManyToManyField(Choice, related_name="questions")
 
     def __str__(self):
         return self.question_text
@@ -34,6 +26,11 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-# class QuestionChoice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
